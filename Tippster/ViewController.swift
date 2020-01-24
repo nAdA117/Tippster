31acf1403413b2b357bc1ed24adc61d9a7f8e977
MITCHELL CZAJA <8000656981@mwce111-03.local>
2020-01-23 19:25:18 -0800

    Initial Commit

---
+++ Tippster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippster
+//
+//  Created by MITCHELL CZAJA on 1/23/20.
+//  Copyright © 2020 College of Southern Nevada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
